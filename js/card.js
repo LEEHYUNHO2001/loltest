3c6187cf424015b92d7cd274755a6d58134f9210
HyunHo Lee <dlgusgh2001@naver.com>
2021-12-01 17:40:32 +0900

    select mediaquery

--- js/card.js
+++ js/card.js
@@ -2,7 +2,7 @@ function selectcard(){
     const line = ["top", "mid", "jung", "sup", "ad"];
     const img = ["../img/card/top.jpg", "../img/card/mid.jpg", "../img/card/jung.jpg",
     "../img/card/sup.jpg", "../img/card/ad.jpg"];
-    const p = ["우직한 탑라이너 vs 정신병자", "바쁜 로밍의 미드 vs 무한 미아핑 ", "리더쉽 정글 vs 삐져서 메이플스토리", "멘탈까지 서포팅 vs 봇 버린 도구", "집중의 카이팅 vs 답없다~ 우물"];
+    const p = ["우직한 탑라이너 <br> vs <br> 탑신병자", "무한 로밍 <br> vs <br> 무한 미아핑 ", "갱단 정글 <br> vs <br> 메이플스토리", "멘탈케어 <br> vs <br> 봇 버린 도구", "집중의 카이팅 <br> vs <br> 답없다~ 우물"];
 
     const ul = document.querySelector(".select-list-items");
     for(var i = 0; i < 5 ; i++){
@@ -11,9 +11,12 @@ function selectcard(){
     li.innerHTML = `
         <li class="select-item-card">
             <div class="select-item select-item${i}">
-                <img src="${img[i]}" alt="카드_앞면">
-                <p>${p[i]}</p>
-                <button onclick="location.href='line/${line[i]}.html'"> 시작하기 </button>
+                <img src="../img/card/front.jpg" alt="카드_앞면">
+                <div class="container">
+                    <img src="${img[i]}" alt="캐릭터">
+                    <p>${p[i]}</p>
+                    <button onclick="location.href='line/${line[i]}.html'"> 시작하기 </button>
+                </div class="container">
             </div>
             <div class="select-item-back select-item${i}-back">
                 <img src="../img/card/card.jpg" alt="카드_뒷면">
