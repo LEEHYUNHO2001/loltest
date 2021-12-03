15dce3645fb9290ffa87f1eca31fed4325009561
HyunHo Lee <dlgusgh2001@naver.com>
2021-12-03 20:23:39 +0900

    result screen

--- README.md
+++ README.md
@@ -4,9 +4,14 @@
 - Bootstrap 버튼
 - mediaquery
 
+
+<br>
+
 ## Deploy
 - netilfy
 - gabia
+
+<br>
 
 서비스 링크 : https://loltest.shop/
 Velog 링크 : https://velog.io/@leehyunho2001/%EB%A1%A4-%EC%9D%B8%EC%84%B1-%ED%85%8C%EC%8A%A4%ED%8A%B8
--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 function result() {
-    // yes의 갯수를 세기위한 변수를 하나 선언함
     var yes_counter = 0; 
-    // 페이지에서 yes인 클래스를 가진 콤포넌트들을 다 참아서 elements에 저장
     var elements = document.getElementsByClassName("yes");
-    // 이 elements들을 하나씩 확인하기
     for (var i=0; i<elements.length; i++) {
-        // i번째 element가 check 되어 있는지 확인하기
         if (elements[i].checked) {
-            // yes_counter를 1 증가
             yes_counter++;
         }
     }
@@ -19,8 +14,7 @@ function result() {
             maybe_counter++;
         }
     }
-    // 대답안한 문항이 있는지 확인하기
-    // no라고 대답한 갯수까지 합쳐서 5이면 모든 질문에 대답했다는 의미
+
     var no_counter = 0;
     elements = document.getElementsByClassName("no");
     for (var i=0; i<elements.length; i++) {
@@ -29,23 +23,52 @@ function result() {
         }
     }
 
-    if (yes_counter + no_counter + maybe_counter !=5) {
+    if (yes_counter + no_counter + maybe_counter !=11) {
         alert("선택안한 문제가 있습니다. 모든 문제에 답해주세요.");
         return;
     }
 
-    var sum = yes_counter + Math.floor(maybe_counter / 2.0);
+    var sum = yes_counter + Math.floor(maybe_counter / 2.0) - 1;
 
-    if (sum == 5)
-        location.href="result/trash.html";
-    else if (sum == 4)
-        location.href="result/stone.html";
-    else if (sum == 3)
-        location.href="result/iron.html";
-    else if (sum == 2)
-        location.href="result/bronze.html";
-    else if (sum == 1)
-        location.href="result/silver.html";
-    else
-        location.href="result/gold.html";
+    var cell = document.getElementById("cell"); while ( cell.hasChildNodes() ) { cell.removeChild( cell.firstChild ); }
+    const result_btn = document.getElementById('cell');
+    const test_section = document.getElementById('cell');
+    const result_art = document.createElement("article");
+    const result_img = ["../../img/lol/challenger.png", "../../img/lol/grandmaster.png", "../../img/lol/master.png", "../../img/lol/diamond.png", "../../img/lol/platinum.png", "../../img/lol/gold.png", "../../img/lol/silver.png", "../../img/lol/bronze.png", "../../img/lol/iron.png", "../../img/lol/stone.jpg", "../../img/lol/trash.jpg"];
+    const result = ['첼린저', '그랜드마스터', '마스터', '다이아', '플레티넘', '골드', '실버', '브론즈', '아이언', '스톤즈', '쓰레기'];
+
+    result_btn.addEventListener('click',  () => {
+        test_section.classList.add('result_section');
+    });
+
+    result_art.className = "result_art";
+    result_art.innerHTML = `
+        <h3 class="h3-result">당신의 인성 티어는 ${result[sum]} 입니다.</h3>
+        <img class="img-result" src=${result_img[sum]} />
+    
+    `;
+    test_section.appendChild(result_art);  
+
+    // if (sum == 10)
+    //     location.href="result/trash.html";
+    // else if (sum == 9)
+    //     location.href="result/stone.html";
+    // else if (sum == 8)
+    //     location.href="result/iron.html";
+    // else if (sum == 7)
+    //     location.href="result/bronze.html";
+    // else if (sum == 6)
+    //     location.href="result/silver.html";
+    // else if (sum == 5)
+    //      location.href="result/gold.html";
+    // else if (sum == 4)
+    //      location.href="result/gold.html";
+    // else if (sum == 3)
+    //      location.href="result/gold.html";
+    // else if (sum == 2)
+    //      location.href="result/gold.html";
+    // else if (sum == 1)
+    //      location.href="result/gold.html";
+    // else
+    //      location.href="result/gold.html";
 }
